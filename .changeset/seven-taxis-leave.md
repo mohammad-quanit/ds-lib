41a3b0890a2c430cca22e0e478503deafaa9e25a
Mohammad-Quanit <muhammadquanit@gmail.com>
2023-12-24 23:13:26 +0500

    add: changeset config for publish package

---
+++ .changeset/seven-taxis-leave.md
@@ -0,0 +1,5 @@
+---
+"ds-lib": patch
+---
+
+initial commit
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export const Hello = () => {
-  console.log("Hello from DS lib")
+export class Queue {
+  private queue: any[]
+  constructor() {
+    this.queue = [];
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  enqueue(item: any) {
+    this.queue.push(item);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      throw new Error('Queue is empty');
+    }
+    this.queue.shift();
+  }
 }
